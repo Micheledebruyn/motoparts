7c86d3180ae72f015cad350a98356c027a60403c
michele de bruyn <debruyn.mm@gmail.com>
2020-01-29 13:31:45 +0100

    add first components being Footer and Navbar

---
+++ motoparts-frontend/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="logo">
+      PLOP
+    </div>
+    <div class="nav-items">
+      <ul>
+        <li>About</li>
+        <li>Shop</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  </div>
+</template>
--- motoparts-frontend/pages/index.vue
+++ motoparts-frontend/pages/index.vue
@@ -33,7 +33,7 @@ import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
   }
 }
 </script>
